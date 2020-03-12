e8f86a0d9615a47d22f0a03ac7abba738543a53e
unknown <louis.wicket@gmail.com>
2020-03-12 16:37:56 +0100

    Suite du parcours

--- Base/4.1. Routage [DÉBUT].js
+++ Base/4.1. Routage [DÉBUT].js
@@ -1,14 +1,12 @@
 "use strict";
 
-const http = require("http");
-const server = http.createServer();
+const http = require("http"); /* Récupère le module http */
+const server = http.createServer(); /* Crée une nouvelle instance de http.Server */
 
-server.listen(8080, () => console.log("le serveur a démarré !"));
+server.listen(8080, () => console.log("Serveur démarré !")); /* Écoute le port 8080 */
 
 server.on("request", (request, response) => {
-    console.log(request.url);
-    console.log(request.method);
-    response.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
-    response.write("<p>Salut</p>");
-    response.end("<p>ça va ?</p>");
+    console.log("url: " + request.url);
+    console.log("method: " + request.method);
+    console.log(request.headers);
 });
