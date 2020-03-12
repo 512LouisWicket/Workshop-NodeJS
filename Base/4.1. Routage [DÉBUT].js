88c3908436d97db00138f88bc845a056180b7730
unknown <louis.wicket@gmail.com>
2020-03-12 16:45:26 +0100

    Suite du parcours

--- Base/4.1. Routage [DÉBUT].js
+++ Base/4.1. Routage [DÉBUT].js
@@ -6,6 +6,8 @@ const server = http.createServer(); /* Crée une nouvelle instance de http.Serve
 server.listen(8080, () => console.log("Serveur démarré !")); /* Écoute le port 8080 */
 
 server.on("request", (request, response) => {
+    /* Le code à l’intérieur de ce bloc est exécuté à chaque requête */
+    
     console.log("url: " + request.url);
     console.log("method: " + request.method);
     console.log(request.headers);
